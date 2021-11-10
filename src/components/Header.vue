2d2556df62fa38a66fbae550d8ac83297675df50
Gilberto P. Lima <glima@tecpar.br>
2021-11-10 17:42:25 -0300

    incluindo imagem

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,8 @@
   <div>
       Gilberto Passos Lima
 
-      Inovação & Solução de problemas
-      Praticidade & Sistemas & Automação
+      Inovação &amp; Solução de problemas
+      Praticidade &amp; Sistemas &amp; Automação
   </div>
 </template>
 
@@ -18,5 +18,15 @@ export default class HeaderPage extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  div {
+    position: absolute;
+    width: 1761.16px;
+    height: 624.01px;
+    left: -294.35px;
+    top: 23.49px;
 
+    background: url('../assets/jigsaw.png'), #E2D58B;
+    background-blend-mode: multiply, normal;
+    transform: rotate(-8deg);
+  }
 </style>
