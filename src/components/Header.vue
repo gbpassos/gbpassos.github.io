70789804053853129c2a9f62ded0c8f017adf40a
Gilberto P. Lima <glima@tecpar.br>
2021-11-10 16:01:26 -0300

    commit inicial

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      Gilberto Passos Lima
+
+      Inovação & Solução de problemas
+      Praticidade & Sistemas & Automação
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HeaderPage extends Vue {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header-page/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HeaderPage from '@/components/Header.vue';
+
+@Component({
+  components: {
+    HeaderPage,
+  },
+})
+export default class Home extends Vue {}
+</script>
