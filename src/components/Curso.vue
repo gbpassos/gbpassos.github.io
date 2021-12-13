758fd8db3bc32b050caf4c0bafdae13f440e1636
Gilberto P. Lima <glima@tecpar.br>
2021-12-13 13:14:58 -0300

    incluindo formacao

---
+++ src/components/Curso.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <span class="iconify" v-bind:data-icon="icon"></span>
+        <div class="descricao">{{ ano }}</div>
+        <div class="descricao">{{ descricao }}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Curso extends Vue {
+  @Prop() private descricao!: string;
+  @Prop() private ano!: string;
+  @Prop() private icon!: string;
+}
+</script>
+
+<style lang="scss" scoped>
+    div.container {
+        display: felxbox;
+        flex-direction: column;
+        div.descricao {
+            text-align: center;
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <formacao></formacao>
     <header-page></header-page>
     <oque></oque>
   </div>
@@ -9,11 +10,13 @@
 import { Component, Vue } from 'vue-property-decorator';
 import HeaderPage from '@/components/Header.vue';
 import Oque from '@/components/Oque.vue';
+import Formacao from '@/components/Formacao.vue';
 
 @Component({
   components: {
     HeaderPage,
     Oque,
+    Formacao,
   },
 })
 export default class Home extends Vue {}
