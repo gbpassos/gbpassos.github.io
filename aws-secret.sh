dc0dfe04f30fe872e435b324bb5ca2dcec185b67
gbpassos <94070770+gbpassos@users.noreply.github.com>
2022-05-23 17:18:16 -0300

    substituindo programa instalado por um script python de linha de comando

--- aws-secret.sh
+++ aws-secret.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export token=$(aws secretsmanager get-secret-value --region sa-east-1 --secret-id sanity --query SecretString --output text | jq -r .token)
+export token=$(aws secretsmanager get-secret-value --region sa-east-1 --secret-id sanity --query SecretString --output text | python -c 'import json,sys; print(json.load(sys.stdin)["token"])')
