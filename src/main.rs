c01dbc2cf03014e20c5f64319e0c4af56587b1e2
Gilberto P <gibapassos@gmail.com>
2022-06-25 13:52:24 -0300

    Versão funcional das flechas

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use decks::apresentacao::Apresentacao;
 enum Msg {
     AddOne,
     DoNothing,
+    KeyUp(Event)
 }
 
 struct Model {
@@ -31,11 +32,28 @@ impl Component for Model {
         match msg {
             Msg::AddOne => {
                 self.value += 1;
+                gloo::console::log!(self.value);
                 // the value has changed so we need to
                 // re-render for it to appear on the page
                 true
             }
-            Msg::DoNothing => false
+            Msg::DoNothing => false,
+            Msg::KeyUp(event) => {
+                let jevent = event.dyn_ref::<web_sys::KeyboardEvent>().unwrap();
+                // gloo::console::log!("keyup",jevent.key_code());
+                match jevent.key_code() {
+                    39 => {
+                        self.value += 1;
+                        true
+                    },
+                    37 => {
+                        self.value -= 1;
+                        if self.value < 0 { self.value = 0 };
+                        true
+                    },
+                    _ => false
+                }
+            }
         }
     }
 
@@ -44,8 +62,13 @@ impl Component for Model {
         let link = ctx.link();
         html! {
             <div>
-                <Apresentacao />
-                <Apresentacao />
+                {
+                    match self.value {
+                        0 => html!{ <Apresentacao /> },
+                        1 => html!{ <Apresentacao /> },
+                        _ => html!{}
+                    }
+                }
                 // <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
                 <p>{ self.value }</p>
             </div>
@@ -59,9 +82,12 @@ impl Component for Model {
         }
         let document = gloo::utils::document();
         let link = ctx.link().clone();
+        let cb: Callback<Event>= link.callback(|event: Event| Msg::KeyUp(event));
         let listener = gloo::events::EventListener::new(&document, "keyup", move | event | {
-            let event = event.dyn_ref::<web_sys::KeyboardEvent>().unwrap();
-            gloo::console::log!(event.key_code());
+            cb.emit(event.clone());
+            // link.send_message(Msg::KeyUp(event.clone()));
+            // let event = event.dyn_ref::<web_sys::KeyboardEvent>().unwrap();
+            // gloo::console::log!("listener",event.key_code());
         });
         self.kbd_listener.replace(listener);
     }
